f21249f74a463bbcb0fa19dc9f4aff9b360b4aa8
hassamtariq399 <hassamtariq399@gmail.com>
2022-10-06 15:06:30 +0500

    "Day  4 completed"

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:core';
 import 'package:firstapp/pages/homepage.dart';
 import 'package:firstapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,9 +20,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(primarySwatch: Colors.red,
+      fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(/*brightness: Brightness.dark,*/
       primarySwatch: Colors.amber
+
       ),
       initialRoute: "/home",
       routes: {
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,9 +5,37 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Material(color: Colors.white, child:Column(children: [
+    Image.asset("sources/images/login_image.png",fit: BoxFit.cover,),
 
-    return Scaffold(appBar: AppBar(title: Text("Login Page"),),body: Center(child: Text("Login Page", style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20, color: Colors
-    .orangeAccent), ) , ),
+      SizedBox(height: 25,),
+      Text("Welcome" , style: TextStyle (fontWeight: FontWeight.bold, fontSize: 20,
+      )
+
+      ),
+      SizedBox(height: 25,),
+
+
+
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 32),
+        child: Column(children: [
+          TextFormField(decoration: InputDecoration(hintText: "Enter User Name", labelText: "Username"
+          )
+          ),
+          TextFormField( obscureText: true,decoration: InputDecoration(hintText: "Enter Password", labelText: "Password"
+          )
+          ),
+          SizedBox(height: 25,),
+          ElevatedButton( onPressed: ()
+            {
+              print("Login Button Clicked");
+            }, child: Text("Login"), style: TextButton.styleFrom(),)
+
+        ],),
+      )
+    ]),
+
 
 
     );
